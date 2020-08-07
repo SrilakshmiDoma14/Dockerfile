9fbc048305865148bd946beaf485a1cb16752c37
Srilakshmi Doma <60988616+SrilakshmiDoma14@users.noreply.github.com>
2020-08-07 12:12:10 -0400

    Adding Tanmai changes

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
 docker build -t tfimage:v1 .
 docker run -it -d --name tcont1 tfimage:v1
-docker exec -it tcont1 bash -c 'cd /root/TFCode/TF_Files && chmod 755 /root/TFCode/TF_Files/terraform.sh && ./terraform.sh; exec "${SHELL:-sh}"'
+docker exec -it tfcontainer bash -c 'cd /root/TFCode/TF_Files && 
+                                     terraform init && 
+                                     terraform plan && 
+                                     terraform apply &&
+                                     docker stop tfcontainer &&
+                                     docker rm tfcontainer && 
+                                     docker rmi tfimage:v1 ; exec "${SHELL:-sh}"'
